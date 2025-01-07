8a879ab661cfaef717cd731a8928cc241bcb508b
sungyong ko <bluedevstorm@gmail.com>
2025-01-07 20:25:14 +0900

    기타: home 버튼 추가

--- client/app/(client)/layout.tsx
+++ client/app/(client)/layout.tsx
@@ -1,9 +1,28 @@
 import AuthProvider from "@/providers/authProvider";
+import Link from "next/link";
 
 export default async function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <div>
+      <Link
+        href="/home"
+        style={{
+          width: "24px",
+          height: "24px",
+          color: "black",
+          position: "fixed",
+          bottom: "20px",
+          right: "100px",
+          zIndex: 1000,
+        }}
+      >
+        Home
+      </Link>
+      <AuthProvider>{children}</AuthProvider>
+    </div>
+  );
 }
