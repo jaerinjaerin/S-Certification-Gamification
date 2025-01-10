a19c060e84815ea8888634831820a4542f7613ad
leejaelll <jayaom91076@gmail.com>
2025-01-10 19:05:25 +0900

    수정: 데이터 요청 실패 시 에러 메세지 수정

--- client/i18n/request.ts
+++ client/i18n/request.ts
@@ -3,13 +3,23 @@ import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
-  const messages = await fetch(
-    `${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s24/messages/${locale}.json`
-  ).then((res) => res.json());
 
-  return {
-    locale,
-    messages: messages,
-    now: new Date(),
-  };
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s24/messages/${locale}.json`
+  );
+
+  if (response.ok) {
+    const messages = await response.json();
+    return {
+      locale,
+      messages: messages,
+      now: new Date(),
+    };
+  } else {
+    const data = await response.json();
+    console.log("DATA", data);
+    throw new Error(
+      `${locale}에 해당하는 JSON 데이터를 가져오는데 문제가 발생했습니다.`
+    );
+  }
 });
