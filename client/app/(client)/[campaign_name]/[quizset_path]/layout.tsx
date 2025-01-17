bbf7f1655a751cb7fb70d8ac618a64f03708fb6c
sungyong ko <bluedevstorm@gmail.com>
2025-01-17 16:46:25 +0900

    기능: quizSetPath 검증 로직 추가

--- client/app/(client)/[campaign_name]/[quizset_path]/layout.tsx
+++ client/app/(client)/[campaign_name]/[quizset_path]/layout.tsx
@@ -1,13 +1,29 @@
+import { defaultLanguageCode } from "@/core/config/default";
 import AuthProvider from "@/providers/authProvider";
+import { validateAndCorrectQuizSetPath } from "@/services/quizService";
 import { NextIntlClientProvider } from "next-intl";
+import { redirect } from "next/navigation";
 
 export default async function SumtotalUserLayout({
   children,
-  params: { quizset_path },
+  params: { campaign_name, quizset_path },
 }: {
   children: React.ReactNode;
-  params: { quizset_path: string };
+  params: { campaign_name: string; quizset_path: string };
 }) {
+  const { validatedPath, isValid, wasCorrected } =
+    await validateAndCorrectQuizSetPath(quizset_path, defaultLanguageCode);
+
+  if (!isValid) {
+    console.error("Path validation failed.");
+    redirect("/error");
+  } else if (wasCorrected) {
+    console.log(`Path was corrected to: ${validatedPath}`);
+    redirect(`/${campaign_name}/${validatedPath}`);
+  } else {
+    console.log("Path is valid.");
+  }
+
   console.log("SumtotalUserLayout quizset_path", quizset_path);
   const timeZone = "Seoul/Asia";
   const locale = quizset_path.split("_").at(-1);
