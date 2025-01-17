b03d5b5c60420c2e812aaa9e371895fcbc345711
leejaelll <jayaom91076@gmail.com>
2025-01-17 15:25:25 +0900

    수정: 코드 수정

--- client/app/(client)/[campaign_name]/[quizset_path]/layout.tsx
+++ client/app/(client)/[campaign_name]/[quizset_path]/layout.tsx
@@ -1,5 +1,3 @@
-import { defaultLocale } from "@/i18n/config";
-import { fetchSupportedLanguages } from "@/i18n/locale";
 import AuthProvider from "@/providers/authProvider";
 import { NextIntlClientProvider } from "next-intl";
 
@@ -12,16 +10,7 @@ export default async function SumtotalUserLayout({
 }) {
   console.log("SumtotalUserLayout quizset_path", quizset_path);
   const timeZone = "Seoul/Asia";
-  const getValidLocale = async () => {
-    const quizsetPathLocale = quizset_path.split("_").at(-1);
-    const supportedLanguagesCode = await fetchSupportedLanguages();
-
-    if (supportedLanguagesCode.find((lang) => lang === quizsetPathLocale))
-      return quizsetPathLocale;
-
-    return defaultLocale;
-  };
-  const locale = await getValidLocale();
+  const locale = quizset_path.split("_").at(-1);
 
   const messages = await fetch(
     `${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s24/messages/${locale}.json`
