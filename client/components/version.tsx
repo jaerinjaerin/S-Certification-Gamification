132203b68d478f77bb07652ffff0f7102db82d62
sungyong ko <bluedevstorm@gmail.com>
2025-01-19 22:34:27 +0900

    버그: version 참조 오류 수정

--- client/components/version.tsx
+++ client/components/version.tsx
@@ -5,7 +5,7 @@ import process from "process";
 import packageJson from "@/package.json";
 
 export function Version() {
-  const env = (process.env.NEXT_PUBLIC_ENV ?? "default").toUpperCase();
+  const env = (process.env.ENV ?? "default").toUpperCase();
   const version = packageJson.version;
   const label = "Environment: " + env + ", Version:" + version;
   console.info(label);
