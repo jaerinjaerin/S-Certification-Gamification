3e66f903cedcdf6079635b9d7e9e6dec72fe61c6
sungyong ko <bluedevstorm@gmail.com>
2025-01-24 19:14:54 +0900

    기능: 스위스 프랑스어 activity id 검증 로직 보완

--- client/providers/quizProvider.tsx
+++ client/providers/quizProvider.tsx
@@ -329,8 +329,9 @@ export const QuizProvider = ({
 
     if (
       _quizLog?.quizSetPath?.toLocaleLowerCase() ===
-      ("NAT_2756_fr-FR"?.toLocaleLowerCase() ||
-        "NAT_2756_fr-FR-ch"?.toLocaleLowerCase())
+        "NAT_2756_fr-FR"?.toLocaleLowerCase() ||
+      _quizLog?.quizSetPath?.toLocaleLowerCase() ===
+        "NAT_2756_fr-FR-ch"?.toLocaleLowerCase()
     ) {
       if (currentQuizStageIndex === 2) {
         return "252558";
