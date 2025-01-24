893b0eb24e578afe11491e71a90c27395c731930
sungyong ko <bluedevstorm@gmail.com>
2025-01-24 20:46:38 +0900

    배지 로그 저장 오류 수정

--- client/app/api/languages/route.ts
+++ client/app/api/languages/route.ts
@@ -6,7 +6,7 @@ import * as Sentry from "@sentry/nextjs";
 export async function GET() {
   try {
     const languages = await prisma.language.findMany();
-    console.log("languages");
+    // console.log("languages");
     const response = NextResponse.json({ items: languages }, { status: 200 });
     // response.headers.set("Cache-Control", "public, max-age=3600");
     return response;
