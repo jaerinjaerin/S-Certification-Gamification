41704cf865edcbe4b176b38c8c0a25be445d3a31
leejaelll <jayaom91076@gmail.com>
2025-01-27 11:01:34 +0900

    수정: 로그인페이지 팝업 수정

--- client/app/(client)/[campaign_name]/[quizset_path]/(auth)/login/page.tsx
+++ client/app/(client)/[campaign_name]/[quizset_path]/(auth)/login/page.tsx
@@ -9,64 +9,17 @@ import useCheckLocale from "@/hooks/useCheckLocale";
 import { cn, isSheetLanguage } from "@/utils/utils";
 import { AutoTextSize } from "auto-text-size";
 import { signIn, useSession } from "next-auth/react";
-<<<<<<< HEAD
-import { useTranslations } from "next-intl";
-
-// import {
-//   Sheet,
-//   SheetClose,
-//   SheetContent,
-//   SheetDescription,
-//   SheetFooter,
-//   SheetHeader,
-//   SheetTitle,
-//   SheetTrigger,
-// } from "@/components/ui/sheet";
-// import {
-//   Accordion,
-//   AccordionContent,
-//   AccordionItem,
-//   AccordionTrigger,
-// } from "@/components/ui/accordion";
-// import { Checkbox } from "@/components/ui/checkbox";
-// import { Markdown } from "@/components/markdown/markdown";
-
-// import { z } from "zod";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-// } from "@/components/ui/form";
-// import { useState } from "react";
-
-// const FormSchema = z.object({
-//   privacy: z.boolean().default(false),
-//   term: z.boolean().default(false),
-// });
-// import { usePolicy } from "@/providers/policyProvider";
-
-=======
 import { useLocale, useTranslations } from "next-intl";
 import { usePolicy } from "@/providers/policyProvider";
 import PolicySheet from "@/components/login/policy-sheet";
 import { useState } from "react";
 
->>>>>>> feature/accept-popup
 export default function Login() {
   useGAPageView();
   const { status } = useSession();
   const translation = useTranslations();
   const { isArabic } = useCheckLocale();
   const { loading, setLoading, renderLoader } = useLoader();
-<<<<<<< HEAD
-  // const { subsidiary } = usePolicy();
-  // const regionName = subsidiary && subsidiary.region.name;
-  // const ACCEPT_POLICY_REGION = regionName === "MENA";
-=======
   const {
     subsidiary,
     privacyContent,
@@ -82,7 +35,6 @@ export default function Login() {
   const PRIVACY_CONTENT = agreementContent
     ? `${agreementContent} === ${privacyContent}`
     : privacyContent;
->>>>>>> feature/accept-popup
 
   const processSignIn = async () => {
     setLoading(true);
@@ -117,23 +69,7 @@ export default function Login() {
                   {translation("be_a_galaxy_ai_expert")}
                 </AutoTextSize>
               </div>
-<<<<<<< HEAD
-              <Button
-                variant={"primary"}
-                disabled={loading}
-                onClick={processSignIn}
-                className={cn(
-                  "disabled:bg-disabled ",
-                  isArabic && "flex-row-reverse"
-                )}
-              >
-                <span>S+</span>
-                <span>{translation("login")}</span>
-              </Button>
-              {/* {!ACCEPT_POLICY_REGION && (
-=======
               {!isPolicyAcceptCountry && (
->>>>>>> feature/accept-popup
                 <Button
                   variant={"primary"}
                   disabled={loading}
@@ -146,13 +82,8 @@ export default function Login() {
                   <span>S+</span>
                   <span>{translation("login")}</span>
                 </Button>
-<<<<<<< HEAD
-              )} */}
-              {/* {ACCEPT_POLICY_REGION && (
-=======
               )}
               {isPolicyAcceptCountry && (
->>>>>>> feature/accept-popup
                 <PolicySheet
                   isSheetLanguage={isSheetLanguage(locale)}
                   processSignIn={processSignIn}
@@ -175,7 +106,7 @@ export default function Login() {
                     <span>{translation("login")}</span>
                   </Button>
                 </PolicySheet>
-              )} */}
+              )}
             </div>
             <PolicyFooter />
           </div>
@@ -185,200 +116,3 @@ export default function Login() {
     </>
   );
 }
-<<<<<<< HEAD
-
-// function PolicySheet({
-//   children,
-//   ACCEPT_POLICY_REGION,
-//   processSignIn,
-//   loading,
-// }: {
-//   children: React.ReactNode;
-//   ACCEPT_POLICY_REGION: boolean;
-//   processSignIn: () => Promise<void>;
-//   loading: boolean;
-// }) {
-//   const [openSheet, setOpenSheet] = useState(ACCEPT_POLICY_REGION);
-//   const { privacyContent, termContent, domainName } = usePolicy();
-//   const translation = useTranslations();
-//   const { isArabic } = useCheckLocale();
-
-//   const form = useForm<z.infer<typeof FormSchema>>({
-//     resolver: zodResolver(FormSchema),
-//     defaultValues: {
-//       privacy: false,
-//       term: false,
-//     },
-//   });
-
-//   const checkAllCheckbox = () => {
-//     if (form.getValues("privacy") && form.getValues("term")) {
-//       return true;
-//     }
-//     return false;
-//   };
-//   const isAllChecked = checkAllCheckbox();
-
-//   return (
-//     <Sheet open={openSheet} onOpenChange={setOpenSheet}>
-//       <SheetTrigger asChild>{children}</SheetTrigger>
-//       <SheetContent
-//         side={"bottom"}
-//         className={cn(
-//           "w-full h-fit max-w-[412px] mx-auto ",
-//           "flex flex-col justify-end"
-//         )}
-//       >
-//         <SheetHeader>
-//           <SheetTitle aria-hidden className="hidden"></SheetTitle>
-//           <SheetDescription>
-//             <Accordion type="single" collapsible defaultValue="privacy">
-//               <Form {...form}>
-//                 <form>
-//                   <FormField
-//                     key={"privacy"}
-//                     control={form.control}
-//                     name="privacy"
-//                     render={({ field }) => {
-//                       return (
-//                         <AccordionItem value="privacy">
-//                           <AccordionTitle
-//                             title={translation("privacy")}
-//                             isArabic={isArabic}
-//                           />
-//                           <AccordionContent className="h-[60svh]  flex flex-col gap-3">
-//                             <div className="overflow-hidden overflow-y-scroll">
-//                               <Markdown
-//                                 className={cn(
-//                                   "text-xs",
-//                                   isArabic && "text-right",
-//                                   domainName === "Myanmar" && "leading-loose"
-//                                 )}
-//                               >
-//                                 {privacyContent}
-//                               </Markdown>
-//                             </div>
-
-//                             <div className="flex items-center space-x-2">
-//                               <FormItem
-//                                 key={"privacy"}
-//                                 className="flex flex-row items-start space-x-3 space-y-0 w-full"
-//                               >
-//                                 <FormControl>
-//                                   <Checkbox
-//                                     checked={field.value}
-//                                     onCheckedChange={(checked) => {
-//                                       field.onChange(checked);
-//                                     }}
-//                                   />
-//                                 </FormControl>
-//                                 <FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-[#0F0F0F]">
-//                                   {translation("mena_check_1")}
-//                                 </FormLabel>
-//                               </FormItem>
-//                             </div>
-//                           </AccordionContent>
-//                         </AccordionItem>
-//                       );
-//                     }}
-//                   />
-//                   <FormField
-//                     key={"term"}
-//                     control={form.control}
-//                     name="term"
-//                     render={({ field }) => {
-//                       return (
-//                         <AccordionItem value="term">
-//                           <AccordionTitle
-//                             title={translation("term")}
-//                             isArabic={isArabic}
-//                           />
-//                           <AccordionContent className="h-[60svh]  flex flex-col gap-3">
-//                             <div className="overflow-hidden overflow-y-scroll">
-//                               <Markdown
-//                                 className={cn(
-//                                   "text-xs",
-//                                   isArabic && "text-right",
-//                                   domainName === "Myanmar" && "leading-loose"
-//                                 )}
-//                               >
-//                                 {termContent}
-//                               </Markdown>
-//                             </div>
-
-//                             <div
-//                               className={cn(
-//                                 "flex items-center space-x-2",
-//                                 isArabic && "justify-end"
-//                               )}
-//                             >
-//                               <FormItem
-//                                 key={"term"}
-//                                 className={cn(
-//                                   "flex flex-row items-start gap-3 space-y-0 ",
-//                                   isArabic && "flex-row-reverse"
-//                                 )}
-//                               >
-//                                 <FormControl>
-//                                   <Checkbox
-//                                     checked={field.value}
-//                                     onCheckedChange={(checked) => {
-//                                       field.onChange(checked);
-//                                     }}
-//                                   />
-//                                 </FormControl>
-//                                 <FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-[#0F0F0F]">
-//                                   {translation("mena_check_2")}
-//                                 </FormLabel>
-//                               </FormItem>
-//                             </div>
-//                           </AccordionContent>
-//                         </AccordionItem>
-//                       );
-//                     }}
-//                   />
-//                 </form>
-//               </Form>
-//             </Accordion>
-//           </SheetDescription>
-//         </SheetHeader>
-//         <SheetFooter className="mt-[26px]">
-//           <Button
-//             variant={"primary"}
-//             disabled={loading || !isAllChecked}
-//             onClick={processSignIn}
-//             className="text-sm"
-//           >
-//             <span>{translation("accept")}</span>
-//           </Button>
-//           <SheetClose asChild>
-//             <Button variant={"primary"} disabled={loading} className="text-sm">
-//               <span>{translation("mena_Decline")}</span>
-//             </Button>
-//           </SheetClose>
-//         </SheetFooter>
-//       </SheetContent>
-//     </Sheet>
-//   );
-// }
-
-// function AccordionTitle({
-//   title,
-//   isArabic,
-// }: {
-//   title: string;
-//   isArabic: boolean;
-// }) {
-//   return (
-//     <AccordionTrigger
-//       className={cn(
-//         "text-xl font-bold text-[#0F0F0F]",
-//         isArabic && "flex-row-reverse"
-//       )}
-//     >
-//       {title}
-//     </AccordionTrigger>
-//   );
-// }
-=======
->>>>>>> feature/accept-popup
