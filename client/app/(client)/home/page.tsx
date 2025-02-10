acb4f0bc241175d135beaad397dc6eff8f27a470
sungyong ko <bluedevstorm@gmail.com>
2025-02-10 21:50:58 +0900

    기능: refresh 버튼, 홈화면 배경 이미지 추가

--- client/app/(client)/home/page.tsx
+++ client/app/(client)/home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Spinner from "@/components/ui/spinner";
 import { AuthType } from "@prisma/client";
 // import { useLocalStorage } from "@/providers/local_storage_provider";
 import { signOut, useSession } from "next-auth/react";
@@ -33,38 +34,49 @@ export default function Home() {
   };
 
   if (status === "loading") {
-    return <p>Loading...</p>;
+    return <Spinner />;
   }
 
   if (session) {
     return (
+      // <div
+      //   style={{
+      //     backgroundImage: `url('${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s25/images/background/main_bg2.jpg')`,
+      //     backgroundSize: "cover",
+      //     backgroundRepeat: "no-repeat",
+      //     backgroundPosition: "center",
+      //   }}
+      // >
+      //   :) Signed in as {session.user?.email} {session.user?.name}
+      //   <br />
+      //   <button onClick={processSignOut}>Sign out</button>
+      //   <br />
+      //   <br />
+      // </div>
+
       <div
+        className="h-full bg-[#F0F0F0] w-full min-h-svh mx-auto text-base flex flex-col justify-center space-y-[19px]"
         style={{
-          backgroundImage: `url('${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s25/images/background/main_bg2.jpg')`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
+          backgroundImage: `url('${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s25/images/bg_main2.jpg')`,
         }}
       >
-        :) Signed in as {session.user?.email} {session.user?.name}
-        <br />
-        <button onClick={processSignOut}>Sign out</button>
-        <br />
-        <br />
+        <h1 className="text-xl text-center text-[#2686F5]">
+          :) Signed in as {session.user?.email} {session.user?.name}
+        </h1>
+
+        <button
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          onClick={processSignOut}
+        >
+          Sign out
+        </button>
       </div>
     );
   }
 
   return (
-    <div
-      style={{
-        backgroundImage: `url('${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s25/images/background/main_bg2.jpg')`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-      }}
-    >
-      <p>Not signed in</p>
+    <div className="min-w-[280px] max-w-[412px] w-full min-h-svh mx-auto text-base flex flex-col justify-center space-y-[19px]">
+      <h1 className="text-xl text-center text-[#2686F5]">Not signed in</h1>
     </div>
   );
 }
--- client/components/error/refresh-button.tsx
+++ client/components/error/refresh-button.tsx
@@ -6,18 +6,21 @@ export default function RefreshButton() {
   const router = useRouter();
 
   return (
-    <div className="min-w-[280px] max-w-[412px] w-full min-h-svh mx-auto text-base flex flex-col justify-center space-y-[19px]">
-      <>
-        <h1 className="text-xl text-center text-[#2686F5]">
-          The network is unstable. Please try again later.
-        </h1>
-        <button
-          onClick={() => router.refresh()}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Retry
-        </button>
-      </>
+    <div
+      className="h-full bg-[#F0F0F0] w-full min-h-svh mx-auto text-base flex flex-col justify-center space-y-[19px]"
+      style={{
+        backgroundImage: `url('${process.env.NEXT_PUBLIC_ASSETS_DOMAIN}/certification/s25/images/bg_main2.jpg')`,
+      }}
+    >
+      <h1 className="text-xl text-center text-[#2686F5]">
+        The network is unstable. Please try again later.
+      </h1>
+      <button
+        onClick={() => router.refresh()}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Retry
+      </button>
     </div>
   );
 }
