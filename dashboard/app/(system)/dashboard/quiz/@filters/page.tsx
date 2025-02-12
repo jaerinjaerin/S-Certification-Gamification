0429069582991ddeeacf035009c4c9a14bcb16bb
Ron Kim <ron@bien.ltd>
2025-02-12 22:59:59 +0900

    기능:SearchParams적용 및 user/domain데이터 최적화

--- dashboard/app/(system)/dashboard/quiz/@filters/page.tsx
+++ dashboard/app/(system)/dashboard/quiz/@filters/page.tsx
@@ -1,12 +1,17 @@
-"use client";
-import Filters from "@/app/(system)/dashboard/_components/filters";
-import { FieldValues } from "react-hook-form";
-import { useQuizContext } from "../_provider/provider";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+import Filters from '@/app/(system)/dashboard/_components/filters';
+import { FieldValues } from 'react-hook-form';
+import { useQuizContext } from '../_provider/provider';
+import { updateSearchParamsOnUrl } from '@/lib/url';
 
 const QuizFilterForm = () => {
   const { dispatch } = useQuizContext();
-  const onSubmit = (formData: FieldValues) => {
-    dispatch({ type: "SET_FIELD_VALUES", payload: formData });
+  const onSubmit = (formData: FieldValues, action?: boolean) => {
+    // 서치파람 업데이트
+    updateSearchParamsOnUrl(formData);
+    //
+    dispatch({ type: 'SET_FIELD_VALUES', payload: formData });
   };
 
   return <Filters onSubmit={onSubmit} />;
