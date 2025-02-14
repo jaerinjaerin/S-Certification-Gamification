1eec226cf5825fb6a7ff5575f3ef3b1856ada119
jane <jane@bien.ltd>
2025-02-14 18:31:07 +0900

    기능: set Quiz 페이지 레이아웃 구현

---
+++ dashboard/app/(system)/cms/loading.tsx
@@ -0,0 +1,7 @@
+import Loader from '@/components/loader';
+
+export default function Loading() {
+  return (
+    <Loader className=" w-full py-28 flex items-center justify-center bg-red-500/80 rounded-xl" />
+  );
+}
--- dashboard/app/(system)/cms/set-quiz/page.tsx
+++ dashboard/app/(system)/cms/set-quiz/page.tsx
@@ -1,3 +1,131 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { DownloadFileListPopoverButton } from '../_components/custom-popover';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useState } from 'react';
+import { ColumnDef } from '@tanstack/react-table';
+import { DataTable } from './data-table';
+
+export type sUser = {
+  id: string;
+  status: 'completed' | 'In Progress';
+  subsidiary: string;
+  domain: string;
+  url: string;
+  quizSet: string;
+  activityId: string;
+  uiLanguage: string;
+};
+
+export type nonSUser = {
+  status: 'completed' | 'In Progress';
+  subsidiary: string;
+  domain: string;
+  url: string;
+  channelName: string;
+  channelSegment: string;
+  jobGroup: string;
+};
+export const sUserColumns: ColumnDef<sUser>[] = [
+  {
+    accessorKey: 'status',
+    header: 'Status',
+  },
+  {
+    accessorKey: 'subsidiary',
+    header: 'Subsidiary',
+  },
+  {
+    accessorKey: 'domain',
+    header: 'Domain',
+  },
+  {
+    accessorKey: 'url',
+    header: 'URL',
+  },
+  {
+    accessorKey: 'quizSet',
+    header: 'Quiz Set',
+  },
+  {
+    accessorKey: 'activityId',
+    header: 'Activity ID',
+  },
+  {
+    accessorKey: 'uiLanguage',
+    header: 'UI Language',
+  },
+];
+
+export const nonSUserColumns: ColumnDef<nonSUser>[] = [
+  {
+    accessorKey: 'status',
+    header: 'Status',
+  },
+  {
+    accessorKey: 'subsidiary',
+    header: 'Subsidiary',
+  },
+  {
+    accessorKey: 'domain',
+    header: 'Domain',
+  },
+  {
+    accessorKey: 'channelName',
+    header: 'URL',
+  },
+  {
+    accessorKey: 'channelSegment',
+    header: 'Channel Segment',
+  },
+  {
+    accessorKey: 'jobGroup',
+    header: 'Job Group',
+  },
+];
 export default function SetQuizPage() {
-  return <div>quiz set page</div>;
+  const [tabState, setTabState] = useState<'s' | 'non-s'>('s');
+
+  return (
+    <div className="flex flex-col">
+      <div className="absolute top-0 right-0 ">
+        <DownloadFileListPopoverButton type="template" />
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <span>Domain List</span>
+          <Tabs
+            defaultValue="s"
+            className="w-[400px]"
+            onValueChange={(value) => {
+              setTabState(value as 's' | 'non-s');
+            }}
+          >
+            <TabsList>
+              <TabsTrigger value="s">S+ Users</TabsTrigger>
+              <TabsTrigger value="non-s">Non S+ Users</TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </div>
+
+        {/* TODO: Need Tab component  */}
+        {/* TODO: Tab의 상태에 따라 버튼 변경  */}
+
+        <div>
+          <DownloadFileListPopoverButton type="data" />
+          <Button variant="action">Bulk Upload</Button>
+        </div>
+      </div>
+      {/* TODO 데이터 테이블 추가 */}
+      {/* TODO: 상태에 따라 테이블이 변경됨 */}
+
+      <div className="my-8">
+        {tabState === 's' ? 'S+ Users' : 'Non S+ Users'}
+      </div>
+      {tabState === 's' && <DataTable columns={sUserColumns} data={[]} />}
+      {tabState === 'non-s' && (
+        <DataTable columns={nonSUserColumns} data={[]} />
+      )}
+    </div>
+  );
 }
