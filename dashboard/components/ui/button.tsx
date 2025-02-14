5408c463b1fe794a6b9f7a6a108450e4e7685541
jane <jane@bien.ltd>
2025-02-14 18:27:28 +0900

    패키지: Shadcn/ui collapsible, dialog, tabs install

--- dashboard/components/ui/button.tsx
+++ dashboard/components/ui/button.tsx
@@ -14,11 +14,15 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          ' border-sidebar-border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+          'border-secondary border text-secondary shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        action:
+          'bg-blue-600 text-primary-foreground shadow hover:bg-blue-100 transition-colors duration-200',
+        download:
+          'bg-secondary-foreground text-secondary shadow hover:bg-secondary/80 transition-colors duration-200',
       },
       size: {
         default: 'h-9 px-4 py-2',
