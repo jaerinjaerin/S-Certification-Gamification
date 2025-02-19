fba3245faf844950a88320bb9d68bc0bdfea4c89
jane <jane@bien.ltd>
2025-02-19 17:54:06 +0900

    파일: home -> campaign으로 페이지 이동

--- dashboard/app/(system)/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import CertificationClientComponent from './_components/certification-component';
-
-export default function HomePage() {
-  return (
-    <>
-      <CertificationClientComponent />
-    </>
-  );
-}
