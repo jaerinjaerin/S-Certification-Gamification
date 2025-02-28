fe2144e959dff91bf2ed2750bae2540b79f105d8
sungyong ko <bluedevstorm@gmail.com>
2025-02-28 15:24:28 +0900

    기타: 캠페인 리스트 요청 시, deleted 값 참조하여 전달하도록 수정

--- dashboard/app/api/(system)/cms/campaign/route.ts
+++ dashboard/app/api/(system)/cms/campaign/route.ts
@@ -146,6 +146,7 @@ export async function GET(request: NextRequest) {
 
       where = {
         quizSets: { some: { domain: { id: { in: domainIds } } } },
+        deleted: false,
       };
     }
 
