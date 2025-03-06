fb3328aa23ad92696624682891e3eaf42c254d6f
Ron Kim <ron@bien.ltd>
2025-03-06 20:28:05 +0900

    수정:api dynamic추가

--- dashboard/app/api/(system)/cms/campaign/check-slug/route.ts
+++ dashboard/app/api/(system)/cms/campaign/check-slug/route.ts
@@ -2,6 +2,8 @@ import { ERROR_CODES } from '@/app/constants/error-codes';
 import { prisma } from '@/model/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = request.nextUrl;
--- dashboard/app/api/(system)/cms/language/data/route.ts
+++ dashboard/app/api/(system)/cms/language/data/route.ts
@@ -4,6 +4,8 @@ import { getFromS3 } from '@/lib/s3-client';
 import { NextRequest, NextResponse } from 'next/server';
 import JSZip from 'jszip';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const { searchParams } = request.nextUrl;
