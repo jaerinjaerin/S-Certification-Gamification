106fc87be7e7de2ebc2b480a43612f8707825d67
sungyong ko <bluedevstorm@gmail.com>
2025-03-11 09:39:28 +0900

    기능: 퀴즈 API 추가

--- dashboard/app/api/(system)/cms/quizset/[quizset_id]/route.ts
+++ dashboard/app/api/(system)/cms/quizset/[quizset_id]/route.ts
@@ -41,3 +41,65 @@ export async function DELETE(request: NextRequest) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
 }
+
+type Props = {
+  params: {
+    quizset_id: string;
+  };
+};
+
+export async function GET(request: NextRequest, props: Props) {
+  try {
+    const quizSetId = props.params.quizset_id;
+    const quizSet = await prisma.quizSet.findFirst({
+      where: {
+        id: quizSetId,
+      },
+      include: {
+        language: true,
+        quizStages: {
+          include: {
+            badgeImage: true,
+            questions: {
+              orderBy: {
+                order: 'asc',
+              },
+              include: {
+                options: {
+                  orderBy: {
+                    order: 'asc',
+                  },
+                },
+                backgroundImage: true,
+                characterImage: true,
+              },
+            },
+          },
+          orderBy: {
+            order: 'asc',
+          },
+        },
+      },
+    });
+
+    if (!quizSet) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: {
+            message: '캠페인을 찾을 수 없습니다.',
+          },
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, result: { quizSet } },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    console.error('Error get quizset: ', error);
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
