4afd4cc8c701e21668cb0bc0d64b395bbb2af401
Sewon Ahn <phillip@bien.ltd>
2025-03-14 14:14:44 +0900

    수정: 언어 참조 리스트 DomainWebLanguages 로 변경

--- client/services/api/fetchSupportedLanguageCodes.ts
+++ client/services/api/fetchSupportedLanguageCodes.ts
@@ -6,14 +6,34 @@ import { prisma } from "@/prisma-client";
 // let lastFetchLangTime: number | null = null;
 // const LANG_CACHE_DURATION = 60000; // 60초 (1분) 캐싱
 
-export async function fetchSupportedLanguageCodes(): Promise<string[]> {
+export async function fetchSupportedLanguageCodes(
+  campaignSlug: string,
+): Promise<string[]> {
   try {
-    const languages = await prisma.language.findMany();
+    const campaign = await prisma.campaign.findUnique({
+      where: {
+        slug: campaignSlug,
+      },
+      include: {
+        domainWebLanguages: {
+          include: {
+            language: true, // language 정보 포함하여 가져옴
+          },
+        },
+      },
+    });
 
-    return languages
-      .filter((item) => !item.name.includes("deprecated"))
-      .map((item) => item.code)
+    if (!campaign || !campaign.domainWebLanguages) {
+      throw new Error(
+        `No campaign or domainWebLanguages found for slug ${campaignSlug}`,
+      );
+    }
+    const languageCodes = campaign.domainWebLanguages
+      .map((dwl) => dwl.language.code) // language의 code를 추출
+      .filter((code) => code) // 유효한 코드값만 필터링
       .sort();
+
+    return languageCodes;
   } catch (error) {
     console.error("Failed to fetch supported languages:", error);
     Sentry.captureException(error);
