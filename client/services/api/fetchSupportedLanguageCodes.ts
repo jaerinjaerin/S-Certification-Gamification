10b1cf8dbbbc7b924b3e5ffcd320ee8e72d08cb6
sungyong ko <bluedevstorm@gmail.com>
2025-03-18 12:44:52 +0900

    기타: campaign 검색 로직에 대소문자 구분하지 않도록 수정

--- client/services/api/fetchSupportedLanguageCodes.ts
+++ client/services/api/fetchSupportedLanguageCodes.ts
@@ -1,18 +1,22 @@
 import { defaultLanguages } from "@/core/config/default";
-import * as Sentry from "@sentry/nextjs";
 import { prisma } from "@/prisma-client";
+import * as Sentry from "@sentry/nextjs";
 
 // let cachedLanguages: string[] | null = null;
 // let lastFetchLangTime: number | null = null;
 // const LANG_CACHE_DURATION = 60000; // 60초 (1분) 캐싱
 
 export async function fetchSupportedLanguageCodes(
-  campaignSlug: string,
+  campaignSlug: string
 ): Promise<string[]> {
+  console.log("fetchSupportedLanguageCodes", campaignSlug);
   try {
-    const campaign = await prisma.campaign.findUnique({
+    const campaign = await prisma.campaign.findFirst({
       where: {
-        slug: campaignSlug,
+        slug: {
+          equals: campaignSlug,
+          mode: "insensitive", // 대소문자 구분 없이 검색
+        },
       },
       include: {
         domainWebLanguages: {
@@ -24,8 +28,13 @@ export async function fetchSupportedLanguageCodes(
     });
 
     if (!campaign || !campaign.domainWebLanguages) {
+      console.error(
+        "No campaign or domainWebLanguages found",
+        campaign,
+        campaign?.domainWebLanguages
+      );
       throw new Error(
-        `No campaign or domainWebLanguages found for slug ${campaignSlug}`,
+        `No campaign or domainWebLanguages found for slug ${campaignSlug}`
       );
     }
     const languageCodes = campaign.domainWebLanguages
