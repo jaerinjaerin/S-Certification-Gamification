0c5f0966e8974fc9d5c62f0f2fd3767f00e00ca1
sungyong ko <bluedevstorm@gmail.com>
2025-03-20 17:25:12 +0900

    기능: 사이트 재진입 시, Sumtotal AccessToken 만료 시 로그아웃 처리

--- client/providers/authProvider.tsx
+++ client/providers/authProvider.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { AuthType } from "@prisma/client";
+import { getSession, SessionProvider, signOut } from "next-auth/react";
 
 import React from "react";
 
+const checkSumTotalTokenExpiration = async () => {
+  const session = await getSession();
+  if (session?.user.authType === AuthType.SUMTOTAL) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth/check-expiry?userId=${session.user.id}`
+    );
+
+    if (response.status >= 400 && response.status < 500) {
+      await signOut({
+        redirect: false,
+      });
+    }
+  }
+};
+
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  checkSumTotalTokenExpiration();
   const baseUrl = `${process.env.NEXT_PUBLIC_BASE_PATH}/api/auth`;
   return <SessionProvider basePath={baseUrl}>{children}</SessionProvider>;
-  // return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
